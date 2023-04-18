12a2c4467ef106de5784584999cb09bcbf5713a6
J03B <byucrazyfan@gmail.com>
2023-04-17 23:04:23 -0500

    Added contact and resume pages with updated packages and utility helpes.

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Resume() {
+    return (
+        <div>
+            <p>Click <a href="./assets/files/Joe_Black_Resume.pdf" download>here</a> to download my current Resume</p>
+        </div>
+    );
+}
