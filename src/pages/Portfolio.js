d151ea1dac93b20bbef6e3303af785f541db4e44
J03B <byucrazyfan@gmail.com>
2023-05-22 20:29:12 -0500

    Added GitHub links for each project and fixed lint warnings

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Project from "../components/Project";
 
 export default function Portfolio() {
-  const [projects, setProjects] = useState([
+  const projects = [
     {
       projectTitle: "Fitness Focus",
       projectDescription:
         "Interactive and polished MERN Project to track live workouts using React, Material UI, MongoDB, Express, Apollo, and Heroku.",
       projectLink: "https://fitnessfocus.herokuapp.com/",
+      projectGitHub: "https://github.com/J03B/fitness-focus",
       projectLinkPhrase: "Workout!",
       imgSrc: "./images/fitness-focus.png",
       imgAlt: "shades of teal pour painted artwork",
@@ -17,6 +18,7 @@ export default function Portfolio() {
       projectDescription:
         "Full stack team project to track goals using a backend SQL database and slick front-end UI powered by Heroku.",
       projectLink: "https://express-goal-tracker.herokuapp.com/",
+      projectGitHub: "https://github.com/dhoneck/goal-tracker",
       projectLinkPhrase: "Track My Goals!",
       imgSrc: "./images/erol-ahmed-unsplash.jpg",
       imgAlt: "yeal yucca plant bird-eye view as it begins blooming",
@@ -26,6 +28,7 @@ export default function Portfolio() {
       projectDescription:
         "An Interactive Google Books API search engine. Use it to save books to your personal user account.",
       projectLink: "https://story-seek.herokuapp.com/",
+      projectGitHub: "https://github.com/J03B/story-seeker",
       projectLinkPhrase: "Read Books!",
       imgSrc: "./images/story-seeker.png",
       imgAlt:
@@ -36,6 +39,7 @@ export default function Portfolio() {
       projectDescription:
         "A fun, interactive trivia game implementing numerous server side APIs, compiling multiple sources into one, user-friendly game.",
       projectLink: "https://j03b.github.io/no-mercy-trivia/",
+      projectGitHub: "https://github.com/J03B/no-mercy-trivia",
       projectLinkPhrase: "Test My Trivia Skills!",
       imgSrc: "./images/willian-justen-unsplash.jpg",
       imgAlt:
@@ -46,7 +50,8 @@ export default function Portfolio() {
       projectDescription:
         "Created custom Portal to pull in the forecast for any searchable city.",
       projectLink: "https://j03b.github.io/weather-dashboard/",
-      projectLinkPhrase: "Search Weather Forecasts",
+      projectGitHub: "https://github.com/J03B/weather-dashboard",
+      projectLinkPhrase: "Search Weather",
       imgSrc: "./images/Weather-Portal-Vegas.png",
       imgAlt:
         "Image/screenshot of what the Weather Portal looks like when searching for Las Vegas forecast",
@@ -54,14 +59,15 @@ export default function Portfolio() {
     {
       projectTitle: "Distillation Column",
       projectDescription:
-        "Python code to determine specific values for distillation column operation",
+        "Python code to determine specific values for distillation column operation.",
       projectLink: "./assets/files/Homework 16.ipynb",
-      projectLinkPhrase: "Download Jupyter Notebook",
+      projectGitHub: "https://github.com/J03B?tab=repositories",
+      projectLinkPhrase: "Download",
       imgSrc: "./images/plant-towers.jpg",
       imgAlt:
         "distiallation collumns at a refinery at dawn, cascaded with work lights",
     },
-  ]);
+  ];
 
   return (
     <div className="container">
